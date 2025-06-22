f0e3d970b50679391803b651dac15cdc4630dfe9
kz5ee <kz5ee@yahoo.com>
2025-06-22 11:58:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ export function downloadFile(data, name) {
 
 const hideRedeemed = async () => {
   const checkbox = document.getElementById('hide-redeemed')
-  console.log('Hiding redeemed keys.')
+  console.log('Showing redeemed keys.')
   if (checkbox.checked) {
     checkbox.click()
     await delayOf(250)
